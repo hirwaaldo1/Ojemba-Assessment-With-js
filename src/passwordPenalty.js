159cf6626487541b496159270c7789b0a251a216
hirwaaldo1 <hirwaaldo1@gmail.com>
2023-05-23 12:51:08 +0300

    feat: add change variable name

--- src/passwordPenalty.js
+++ src/passwordPenalty.js
@@ -13,12 +13,12 @@ export default function penaltyPoints(password = "") {
   if (!password || password === "" || password == "null") return 0;
 
   // Check if the password has any repeated characters
-  const findDublpsNumber = password.match(/(\d)\1+/);
-  const findDublpsLetter = password.match(/(\w)\1+/);
+  const duplicateNumbers = password.match(/(\d)\1+/);
+  const duplicateLetters = password.match(/(\w)\1+/);
 
   // Count the number and letter that are repeating
-  let repeatedCharacters = findDublpsNumber ? findDublpsNumber[0].length : 0;
-  repeatedCharacters += findDublpsLetter ? findDublpsLetter[0].length : 0;
+  let repeatedCharacters = duplicateNumbers ? duplicateNumbers[0].length : 0;
+  repeatedCharacters += duplicateLetters ? duplicateLetters[0].length : 0;
 
   return repeatedCharacters === 0 ? 0 : repeatedCharacters <= 2 ? 1 : 2;
 }
